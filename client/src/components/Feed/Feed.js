964084f15cf1f715dc3dfaf1f2415e8f7b2d7330
belikeamitesh <amitesh46237242@gmail.com>
2021-12-26 17:54:25 +0530

    "major changes

--- client/src/components/Feed/Feed.js
+++ client/src/components/Feed/Feed.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState ,useEffect} from 'react';
 import './Feed.css';
+import axios from 'axios';
 import InputOption from './InputOption';
 import Post from './Post';
 import CreateIcon from '@material-ui/icons/Create';
@@ -11,17 +12,34 @@ import FlipMove from 'react-flip-move';
 
 export default function Feed() {
   const [input, setInput] = useState('');
-  const [posts, setPosts] = useState([
-    {
-      id: 'h',
-      data: {
-        name: 'Rituparna',
-        description: 'HueHue',
-        message: 'Lmao ded',
-        photoUrl: 'http://localhost:5000/public/images/none.png',
-      },
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
+
+    useEffect( ()=> {
+    const getPatients = async ()=>{
+        try {
+        const response = await axios.get('http://localhost:5000/api/posts');
+            // console.log(response);
+            const myPatients = response.data;
+            console.log(myPatients);
+            setPosts(myPatients);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    getPatients();
+},[]);
+
+const getPatients = async (id)=>{
+        try {
+            console.log(id);
+        const response = await axios.post(``);
+            console.log(response);
+            // const newp =patients.filter(patient => id!== patient.id);
+            // setPatients(newp);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
   const sendPost = (e) => {
     e.preventDefault();
@@ -60,15 +78,20 @@ export default function Feed() {
 
       {/* Posts */}
       <FlipMove>
-        {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+        {posts.map((post) => (
           <Post
-            key={id}
-            name={name}
-            description={description}
-            message={message}
-            photoUrl={photoUrl}
+            key={post.id}
+            name={post.author.name}
+            email={post["author"]["email"]}
+            description={post["author"]["branch"]}
+            achievement={post["achievement"]}
+            experience={post["exp"]}
+            status={post["status"]}
+            university={post["author"]["university"]}
+            photoUrl={`http:localhost:5000/post["author"]["profile"]`}
           />
-        ))}
+        )
+        )}
       </FlipMove>
     </div>
   );
--- client/src/components/Feed/Post.js
+++ client/src/components/Feed/Post.js
@@ -7,19 +7,27 @@ import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
 import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
 import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
 
-const Post = forwardRef(({ name, description, message, photoUrl }, ref) => {
+const Post = forwardRef(({ name,email,description,achievement,experience,university,photoUrl }, ref) => {
   return (
     <div ref={ref} className="post">
       <div className="post__header">
-        <Avatar src={photoUrl}>{name[0]}</Avatar>
+        <Avatar src={photoUrl}>{name}</Avatar>
         <div className="post__info">
           <h2>{name}</h2>
+          <p>{university}</p>
           <p>{description}</p>
         </div>
       </div>
 
       <div className="post__body">
-        <p>{message}</p>
+        <h3>My Achievements</h3>
+        <hr />
+        <p>{achievement}</p>
+        <br />
+        <h3>My Project/Experieces</h3>
+        <hr />
+        <p>{experience}</p>
+        <br />
       </div>
       <div className="post__buttons">
         <InputOption Icon={ThumbUpIcon} title="Like" color="gray" />
