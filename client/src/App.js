af26eb8fcf215ea8368610471c3140b05fc3f5b7
Amitesh Kumar <56907437+belikeamitesh@users.noreply.github.com>
2021-12-26 13:51:40 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Sidebar from "./components/Sidebar/Sidebar"
+import Feed from "./components/Feed/Feed";
+import Widgets from "./components/Widgets/Widgets";
+
+function App() {
+
+  return (
+    <div>
+        <div className ="app">
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets/>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,53 @@
+import { Avatar } from '@material-ui/core'
+import React from 'react'
+import './Sidebar.css'
+
+export default function Sidebar() {
+
+
+    const recentItem = (topic) => (
+        <div className="sidebar__recentItem">
+            <span className="sidebar__hash">#</span>
+            <p>{topic}</p>
+        </div>
+    ) 
+
+    return (
+        <div className="sidebar">
+            <div className="sidebar__top">
+                <img src="https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-modern-double-color-futuristic-neon-background-image_351866.jpg" alt="background" />
+                <Avatar src="https://image.pngaaa.com/294/1274294-middle.png" className="sidebar__avatar"> 
+                Amitesh</Avatar>
+                <h2>Amitesh Kumar</h2>
+                <h4>amitesh.kumar@iiitg.ac.in</h4>
+            </div>
+            
+            <div className="sidebar__stats">
+                <div className="sidebar__stat">
+                <p>Who viewed your profile</p>
+                <p className="sidebar__statNumber">126</p>
+                </div>
+                <div className="sidebar__stat">
+                <p>Views of your post</p>
+                <p className="sidebar__statNumber">67</p>
+                </div>
+
+            </div>
+
+            <div className="sidebar__bottom">
+                <p>Recent</p>
+                {recentItem('IIT Kanpur')}
+                {recentItem('CSE')}
+                {recentItem('IT')}
+                {recentItem('Google')}
+                {recentItem('Microsoft')}
+                <h4>Followed Hastags</h4>
+                {recentItem('Projects')}
+                {recentItem('Internships')}
+                {recentItem('Jobs')}
+                {recentItem('College')}
+                {recentItem('tech')}
+            </div>
+        </div>
+    )
+}
