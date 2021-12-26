73937b4c102fdfb2aaaf36a62f3e216e64cfe404
Rituparna Warwatkar <rituparnaw16@gmail.com>
2021-12-26 16:40:16 +0530

    added atla

--- server/utils/DB.js
+++ server/utils/DB.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-  return mongoose.connect('mongodb://localhost:27017/edtech');
+  return mongoose.connect('mongodb+srv://notes:FWXjQvXcCAdsPMto@cluster0ritu.adt1u.mongodb.net/edtech?retryWrites=true&w=majority');
 };
+
+
